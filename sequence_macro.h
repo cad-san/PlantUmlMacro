cb33c73eac0ce7e23dc3ed6e5bff33316f82c217
cad-san <cadsan1986@gmail.com>
2014-03-01 23:18:18 +0900

    implement function macros
    - func(a, b, message)
    - func_begin(a,b, message) ~ func_end(a, b)
    - local_func(a, message)
    - local_func_begin(a, message) ~ local_func_end(a)

---
+++ sequence_macro.h
@@ -0,0 +1,37 @@
+'***********************************************
+' function
+'***********************************************
+!definelong func(a,b,message)
+	a -> b : message
+	activate b
+	a <-- b
+	deactivate b
+!enddefinelong
+
+!definelong func_begin(a,b,message)
+	a -> b : message
+	activate b
+!enddefinelong
+
+!definelong func_end(a,b)
+	a <-- b
+	deactivate b
+!enddefinelong
+
+'***********************************************
+' local_func
+'***********************************************
+!definelong local_func(a,message)
+	a -> a : message
+	activate a
+	deactivate a
+!enddefinelong
+
+!definelong local_func_begin(a,message)
+	a -> a : message
+	activate a
+!enddefinelong
+
+!definelong local_func_end(a)
+	deactivate a
+!enddefinelong
