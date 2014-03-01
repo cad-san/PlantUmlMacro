9fc38bc346515aedd1070eae90df77ff77be2464
cad-san <cadsan1986@gmail.com>
2014-03-02 00:18:59 +0900

    add sequence skin header

---
+++ sequence_skin.h
@@ -0,0 +1,20 @@
+skinparam sequence {
+	ArrowColor		#cc3300
+	ArrowFontSize	15
+	ArrowFontStyle	plane
+
+	LifeLineBorderColor		#555555
+	LifeLineBackgroundColor	#ffffff
+
+	ParticipantBackgroundColor	#eeeeee
+	ParticipantBorderColor		#000000
+	ParticipantFontColor		#333333
+	ParticipantFontSize			20
+	ParticipantFontStyle		bold
+
+	ActorBackgroundColor		#eeeeee
+	ActorBorderColor			#000000
+	ActorFontColor				#333333
+	ActorFontSize				15
+	ActorFontStyle 				bold
+}
