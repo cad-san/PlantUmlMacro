4c2c52ba0b4c520db0422ff75522d2663a6ff29e
cad-san <cadsan1986@gmail.com>
2014-09-08 01:48:57 +0900

    remove colon from annotation-macros

--- sequence_macro.h
+++ sequence_macro.h
@@ -19,14 +19,14 @@
 !enddefinelong
 
 !definelong func_anno(x,y,func_name,comment)
-	x -> y : <b>comment:</b>\n\t func_name
+	x -> y : <b>comment</b>\n\t func_name
 	activate y
 	x <-- y
 	deactivate y
 !enddefinelong
 
 !definelong func_begin_anno(x,y,func_name,comment)
-	x -> y : <b>comment:</b>\n\t func_name
+	x -> y : <b>comment</b>\n\t func_name
 	activate y
 !enddefinelong
 
@@ -49,13 +49,13 @@
 !enddefinelong
 
 !definelong local_func_anno(a,func_name,comment)
-	a -> a : <b>comment:</b>\n\t func_name
+	a -> a : <b>comment</b>\n\t func_name
 	activate a
 	deactivate a
 !enddefinelong
 
 !definelong local_func_begin_anno(a,func_name,comment)
-	a -> a : <b>comment:</b>\n\t func_name
+	a -> a : <b>comment</b>\n\t func_name
 	activate a
 !enddefinelong
 
